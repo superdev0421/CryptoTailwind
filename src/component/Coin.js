0513d307e871c1d7d4ffb2e58f3ad65e7c8454d5
chassitynakamura <nakamurachassity@gmail.com>
2022-08-23 18:32:23 -0700

    Draw the table

---
+++ src/component/Coin.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Coin = ({ name, id, image, symbol, price, volume, priceChange, marketcap, ath, ath_date, circulating_supply, max_supply }) => {
+    return (
+        <div>{name} </div>
+    )
+}
+
+export default Coin;
--- src/component/CryptoTable.js
+++ src/component/CryptoTable.js
@@ -2,35 +2,59 @@
 ----------------------------------------
 Title:    App.js
 Date:     Aug 22, 2022
-Author:   Edward Liu
+Author:   Chassity
 ----------------------------------------
 */
 
-import { useQuery } from "react-query";
 
-const fetchData = async () => {
-  const res = await fetch("https://api.coincap.io/v2/assets");
-  return res.json();
-};
+import React, {useState, useEffect} from 'react'
+import { useQuery } from "react-query";
+import Coin from './Coin' 
+import axios from 'axios'
+
+
 
 function CryptoTable() {
-  const { status, isStale, isFetching, error, data } = useQuery(
+  const fetchData = async () => {
+    console.log("asdf");
+    const res = await axios(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
+    console.log('crypto currency', res.data);
+    return res.data;
+  };
+  
+  const { data,  status } = useQuery(
     "cryptoInfo",
-    fetchData
+     fetchData
   );
-  console.log(status, isStale, isFetching, error, data);
+  console.log(status, data);
   return (
     <div>
       <h1>CryptoTable</h1>
-      {status === "error" && <p>Error fetching data...</p>}
-      {status === "loading" && <p>Fetching data...</p>}
-      {status === "success" && (
-        <div>
-          {data.data.map((coin) => (
-            <p key={coin.id}>{coin.name}</p>
-          ))}
-        </div>
-      )}
+        {status === "error" && <p>Error fetching data...</p>}
+        {status === "loading" && <p>Fetching data...</p>}
+        {status === "success" && (
+          <div>
+            {data.map(coin => {
+              return (  
+                <Coin
+                  key={coin.id}
+                  id={coin.id}
+                  name={coin.name}
+                  image={coin.image}
+                  symbol={coin.symbol}
+                  marketcap={coin.market_cap}
+                  price={coin.current_price}
+                  priceChange={coin.price_change_percentage_24h}
+                  volume={coin.total_volume}
+                  ath={coin.ath}
+                  ath_date={coin.ath_date}
+                  circulating_supply={coin.circulating_supply}
+                  max_supply={coin.max_supply}
+                />
+              )
+        })}
+          </div>
+        )}
     </div>
   );
 }
